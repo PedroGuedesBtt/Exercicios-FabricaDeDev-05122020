6a9a4acd20a3c8c6d985acaa1584384a6e790027
PedroGuedesBtt <pedroluanbtt@gmail.com>
2020-12-05 13:35:18 -0300

    Exercicio 2

---
+++ ConsoleApp1/Ex2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+                Console.Write("Entre com um valor numerico: ");
+                var valorColetado = int.TryParse(Console.ReadLine(), out int valorDigitadoColetado);
+
+                if (valorColetado == true)
+                {
+
+                    Console.WriteLine("O Valor Digitado está certo! \nfoi: {0}", valorDigitadoColetado);
+                }
+                else
+                {
+                Console.WriteLine("Infelizmente você não colocou um valor válido...\n{0}",valorColetado, "\n Abra o programa novamente e tente de novo");
+
+                }
+
+             
+         
+
+           
+        }
+    }
+}
