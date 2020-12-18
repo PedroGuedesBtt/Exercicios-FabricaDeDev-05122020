04fa9bf2dbe97c1ad62cbea4eca5f99e48c328c5
PedroGuedesBtt <pedroluanbtt@gmail.com>
2020-12-18 00:35:40 -0300

    Exercicio 1 do dia 12

---
+++ ExerciciosFabricadeDev-05122020/Ex1-12-12-2020/Funçoes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ex1_12_12_2020
+{
+    class Funçoes
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Lampada Luz = new Lampada();
+
+            // Luz.acende();
+           Luz.apaga();
+           Luz.mostraEstado();
+        }
+    }
+}
---
+++ ExerciciosFabricadeDev-05122020/Ex1-12-12-2020/Lampada.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace Ex1_12_12_2020
+{
+     public class Lampada{
+        public string estado;
+          public Lampada ()
+            {
+            estado = "Desligado";
+            }
+       public void acende()
+        {
+            estado = "Ligada";
+        }
+        public void apaga()
+        {
+            estado = "Desligada";
+        }
+        public void mostraEstado()
+        {
+            if (estado == "Ligada")
+            {
+                Console.WriteLine("Lâmpada Acesa!");
+            }
+            else
+            {
+                Console.WriteLine("Lâmpada Apagada");
+            }
+        }
+
+
+        }
+        
+
+
+    }
+
