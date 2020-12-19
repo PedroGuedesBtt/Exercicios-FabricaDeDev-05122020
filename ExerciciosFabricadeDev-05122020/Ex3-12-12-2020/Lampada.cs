6db19d8f942002c00eb48a4715323ecf5b23be3f
PedroGuedesBtt <pedroluanbtt@gmail.com>
2020-12-19 12:28:42 -0300

    Exercicios novos

---
+++ ExerciciosFabricadeDev-05122020/Ex3-12-12-2020/Lampada.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+
+namespace Ex1_12_12_2020
+{
+     public class Lampada{
+
+        public enum estado
+        {
+            Ligado,
+            Desligado
+        }
+        public estado Estado { get; set; }
+        public Lampada ()
+            {
+            Estado = estado.Desligado ;
+            }
+       public void acende()
+        {
+            Estado = estado.Ligado;
+        }
+        public void apaga()
+        {
+            Estado = estado.Desligado;
+        }
+        public void mostraEstado()
+        {
+            if (Estado == estado.Ligado)
+            {
+                Console.WriteLine("Lâmpada Acesa!");
+            }
+            else
+            {
+                Console.WriteLine("Lâmpada Apagada");
+            }
+        }
+
+
+        }
+        
+
+
+    }
+
